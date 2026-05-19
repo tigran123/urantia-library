f2b6cf6eb037dc2b8890fa4362254e0e2f9a791e
Tigran Aivazian <aivazian.tigran@gmail.com>
2026-05-19 23:55:56 +0100

    Remove superfluous commented lines

--- migrate.sh
+++ migrate.sh
@@ -7,8 +7,3 @@ rm -rf /Books/.data
 export PYTHONUNBUFFERED=1
 
 /Books/urantia-library/migrate_library.py --src /Books-test --target /Books --db /Books/urantia-library/webapp/backend/auth.db --exclude-file=/Books-test/exclude.txt
-
-#find /Books -name '.htaccess' -exec rm {} \;
-#find /Books -name '000-browse.php' -exec rm {} \;
-#find /Books -name '.covers' -exec rm -rf {} \;
-#find /Books -type l -lname "*/default-cover.jpg" -exec rm {} \;
