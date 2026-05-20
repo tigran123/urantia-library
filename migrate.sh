0e1dd39b64c3408a7a89686807a5bed9ce28060a
Tigran Aivazian <aivazian.tigran@gmail.com>
2026-05-20 14:42:05 +0100

    Switch to Big Migration from /Books-full

--- migrate.sh
+++ migrate.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-rm -rf /Books/.data
+rm -rf /Books/.data \
+       /Books/Art/ \
+       /Books/Dictionaries/ \
+       /Books/Encyclopedias/ \
+       /Books/fb2/ \
+       /Books/Fiction/ \
+       /Books/Grammars/ \
+       /Books/History/ \
+       /Books/Institut-russkoy-civilizacii/ \
+       /Books/Law/ \
+       /Books/Manuscripts/ \
+       /Books/Music/ \
+       /Books/Recipes/ \
+       /Books/Religions/ \
+       /Books/Science/ \
+       /Books/Urantia/ \
+       /Books/Zhurnaly/
 
 /Books/urantia-library/webapp/backend/initdb.sh
 
 export PYTHONUNBUFFERED=1
 
-/Books/urantia-library/migrate_library.py --src /Books-test --target /Books --db /Books/urantia-library/webapp/backend/auth.db --exclude-file=/Books-test/exclude.txt
+/Books/urantia-library/migrate_library.py --src /Books-full --target /Books --db /Books/urantia-library/webapp/backend/auth.db --exclude-file=/Books-full/exclude.txt
